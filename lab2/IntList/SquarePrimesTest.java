7a26cbe0ce22d86984d0f298e811f470cea8f37d
xianzhe233 <xianzhe233@xianzhe.com>
2024-11-11 21:51:28 +0800

    complete lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesAllPrimes() {
+        IntList lst = IntList.of(2, 3, 5, 7, 11, 13, 17, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121 -> 169 -> 289 -> 361", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesFirstAndLastPrimes() {
+        IntList lst = IntList.of(19, 4, 4, 4, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("361 -> 4 -> 4 -> 4 -> 361", lst.toString());
+        assertTrue(changed);
+    }
 }
