9b34f4bcbc6412a034c97a9545f9c915b72eeacb
xianzhe233 <xianzhe233@qq.com>
2024-11-11 22:25:05 +0800

    try to fix DebugExercise; add empty list condition process for AddConstantTest.

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,18 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+
+    @Test
+    public void testAddToEmptyList() {
+        IntList lst = IntList.of();
+        IntListExercises.addConstant(lst, 2);
+        assertEquals("", (lst == null) ? "" : lst.toString());
+    }
+
+    @Test
+    public void testAddToLengthOneList() {
+        IntList lst = IntList.of(1);
+        IntListExercises.addConstant(lst, 5);
+        assertEquals("6", lst.toString());
+    }
 }
